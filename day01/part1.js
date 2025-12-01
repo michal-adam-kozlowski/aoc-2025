22b3fe73d86ade8c5ee797c50914562319b7b18a
michal-adam-kozlowski <50498212+michal-adam-kozlowski@users.noreply.github.com>
2025-12-01 15:18:19 +0100

    advent of code 2

--- day01/part1.js
+++ day01/part1.js
@@ -1,6 +1,6 @@
 const { readInput, lines } = require("../utils");
 
-const day01part2 = () => {
+const day01part1 = () => {
   let dialPosition = 50;
   const setDialPostion = (value) => (dialPosition = value);
   let zeroCounter = 0;
@@ -8,68 +8,40 @@ const day01part2 = () => {
 
   const dialMin = 0;
   const dialMax = 99;
-  const dialSize = dialMax - dialMin + 1; // 100 pozycji
+  const dialSize = dialMax - dialMin + 1; // 100
 
   const splitCode = (data) => {
-    const direction = data[0].toUpperCase(); // na wszelki wypadek, gdyby było "r"/"l"
-    const value = parseInt(data.slice(1), 10); // dystans
+    const direction = data.slice(0, 1);
+    const value = parseInt(data.slice(1), 10);
 
     return { direction, value };
   };
 
   const wrapDial = (value) => {
-    // zawijanie w zakresie 0..99
     return ((((value - dialMin) % dialSize) + dialSize) % dialSize) + dialMin;
   };
 
-  // liczy ile razy podczas tej rotacji dial wskoczy na 0
-  const countZerosDuringRotation = (direction, distance, startPos) => {
-    const N = dialSize;
-    const d = direction === "R" ? 1 : -1;
-
-    // Szukamy k z zakresu [1, distance], takich że:
-    // (startPos + d * k) % N === 0
-    //
-    // Rozwiązanie daje:
-    // k ≡ -d * startPos (mod N)
-
-    let k0 = (((-d * startPos) % N) + N) % N; // w zakresie 0..N-1
-
-    if (k0 === 0) {
-      k0 = N; // pierwsze trafienie w 0 wypada po N krokach
-    }
-
-    if (k0 > distance) {
-      return 0;
-    }
-
-    // k = k0, k0 + N, k0 + 2N, ...
-    // ile takich k mieści się w [1, distance]?
-    return 1 + Math.floor((distance - k0) / N);
-  };
+  const checkDialMax = (value) => wrapDial(value);
+  const checkDialMin = (value) => wrapDial(value);
 
   const move = (data) => {
     const { direction, value } = splitCode(data);
 
-    // ile razy podczas TEJ rotacji trafiamy w 0?
-    const zerosThisMove = countZerosDuringRotation(
-      direction,
-      value,
-      dialPosition
-    );
-    setZeroCounter(zeroCounter + zerosThisMove);
-
-    // aktualizujemy pozycję końcową (jak w części 1)
     let newDialPosition;
+
     if (direction === "R") {
-      newDialPosition = wrapDial(dialPosition + value);
+      newDialPosition = checkDialMax(dialPosition + value);
     } else if (direction === "L") {
-      newDialPosition = wrapDial(dialPosition - value);
+      newDialPosition = checkDialMin(dialPosition - value);
     } else {
-      return; // ignorujemy bzdurny kierunek
+      return;
     }
 
     setDialPostion(newDialPosition);
+
+    if (newDialPosition === 0) {
+      setZeroCounter(zeroCounter + 1);
+    }
   };
 
   const rawData = readInput("day01/input.txt");
@@ -80,4 +52,4 @@ const day01part2 = () => {
   console.log(zeroCounter);
 };
 
-day01part2();
+day01part1();
